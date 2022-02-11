dbdfdcaa3ea46fb8f3edfbfd4225754c3ba9092a
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 17:38:38 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -8,7 +8,7 @@ require __DIR__ . '/MercadoPago/SDK.php';
 require __DIR__ . '/MercadoPago/RestClient.php';
 require __DIR__ . '/MercadoPago/Http/HttpRequest.php';
 require __DIR__ . '/MercadoPago/Http/CurlRequest.php';
-//require __DIR__ . '/MercadoPago/Http/HttpRequest.php';
+require __DIR__ . '/MercadoPago/Config.php';
 
 // SDK de Mercado Pago
 //use MercadoPago\SDK as MercadoPago\SDK;
