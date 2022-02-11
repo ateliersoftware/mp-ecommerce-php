5bf44702179c29fed1471fccc04f83e6532da2b5
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 15:09:16 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,6 +4,7 @@ spl_autoload_register("MercadoPago/SDK");
 spl_autoload_register(function($className)
 {
     $namespace=str_replace("\\","/",__NAMESPACE__);
+    echo $namespace;
     $className=str_replace("\\","/",$className);
     $class=CORE_PATH."/classes/".(empty($namespace)?"":$namespace."/")."{$className}.class.php";
     include_once($class);
