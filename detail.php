c18c87f70411a8ba30eb83966c32b662f6906811
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 15:38:01 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-$mp = new SDK();
 MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
 ?>
 
