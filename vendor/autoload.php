224725910dfcd2ff2042d3bf36865653f7846f05
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 15:26:22 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,11 +1,11 @@
 <?
-echo "tututut";
-spl_autoload_register(function($className)
+spl_autoload_register('myAutoloader');
+
+function myAutoloader($className)
 {
-    $namespace=str_replace("\\","/",__NAMESPACE__);
-    echo var_dump($namespace);
-    $className=str_replace("\\","/",$className);
-    $class=CORE_PATH."/classes/".(empty($namespace)?"":$namespace."/")."{$className}.class.php";
-    include_once($class);
-});
+    //$path = '/path/to/class/';
+    $path = '';
+
+    include $path.$className.'.php';
+}
 ?>
