2eb3195ce3819b5ac492f8134ae4e62bb415e4a8
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 15:52:25 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,6 +3,7 @@ function __autoload($class)
 {
     // Adapt this depending on your directory structure
     $parts = explode('\\', $class);
+    echo var_dump(end($parts));
     require end($parts) . '.php';
 }
 ?>
