ffad576cb28903ae38f06a35f3e96534675ee7e3
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 17:34:35 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,11 +1,6 @@
 <?php 
 require __DIR__ . '/MercadoPago/SDK.php';
 require __DIR__ . '/MercadoPago/RestClient.php';
-require __DIR__ . '/MercadoPago/Http/CurlRequest';
-/*function __autoload($class)
-{
-    // Adapt this depending on your directory structure
-    $parts = explode('\\', $class);
-    require end($parts) . '.php';
-}*/
+require __DIR__ . '/MercadoPago/Http/CurlRequest.php';
+require __DIR__ . '/MercadoPago/Http/HttpRequest.php';
 ?>
