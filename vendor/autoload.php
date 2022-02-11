b08162eb5c8ae2367292033526c9f14ebc36ea53
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-11 15:14:50 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function($className)
 {
     $namespace=str_replace("\\","/",__NAMESPACE__);
-    echo $namespace;
+    echo var_dump($namespace);
     $className=str_replace("\\","/",$className);
     $class=CORE_PATH."/classes/".(empty($namespace)?"":$namespace."/")."{$className}.class.php";
     include_once($class);
