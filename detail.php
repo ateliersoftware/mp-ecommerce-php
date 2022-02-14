aee586308f1c814041d035647a86fbc1532c518c
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-14 12:01:54 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 //require __DIR__ .  '/vendor/autoload.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/DocParser.php';
 require __DIR__ . '/lib/Doctrine/Common/Annotations/Reader.php';
 require __DIR__ . '/lib/Doctrine/Common/Annotations/AnnotationRegistry.php';
 require __DIR__ . '/lib/Doctrine/Common/Annotations/AnnotationReader.php';
