88ea817933f0682c97f8a0898411241f9fab1e49
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-14 16:27:01 -0300

    Update mp-success.php

--- mp-success.php
+++ mp-success.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 echo "lol";
   $req_dump = print_r($_REQUEST, TRUE);
   $fp = fopen( __DIR__ .'/logs/success.log', 'a');
