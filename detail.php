26c7cb7049b261b710f0098dae76d18fc72196ee
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-14 14:23:28 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -34,6 +34,7 @@ require __DIR__ . '/MercadoPago/Entities/Shared/Item.php';
 
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac13000");
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
