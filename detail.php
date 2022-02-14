a260c22199739133b60a3eb446d2c9689f111447
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-14 11:49:30 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 
 //require __DIR__ .  '/vendor/autoload.php';
 require __DIR__ . '/lib/Doctrine/Common/Annotations/AnnotationRegistry.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/Annotation.php';
 require __DIR__ . '/MercadoPago/SDK.php';
 require __DIR__ . '/MercadoPago/RestClient.php';
 require __DIR__ . '/MercadoPago/Http/HttpRequest.php';
