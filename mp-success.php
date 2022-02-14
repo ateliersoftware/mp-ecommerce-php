7d4e5fca8d9d4786e3c2062c4085d67851b5f2d4
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-14 17:54:08 -0300

    Update mp-success.php

--- mp-success.php
+++ mp-success.php
@@ -3,15 +3,15 @@
   ini_set('display_startup_errors', '1');
   error_reporting(E_ALL);
 echo "lol";
-  $fichero = __DIR__ .'/logs/success.log';
+  $fichero = __DIR__ .'/success.log';
 
   $req_dump = print_r($_REQUEST, TRUE);
-  $fp = fopen( __DIR__ .'/logs/success.log', 'a');
+  $fp = fopen( __DIR__ .'/success.log', 'a');
   fwrite($fp, $req_dump);
   fclose($fp);
 
   $req_dump = print_r($_GET, TRUE);
-  $fp = fopen( __DIR__ .'/logs/success.log', 'a');
+  $fp = fopen( __DIR__ .'/success.log', 'a');
   echo var_dump(fwrite($fp, $req_dump));
   fclose($fp);
 
