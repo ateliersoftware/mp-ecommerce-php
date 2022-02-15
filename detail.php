817a4b09fd7aa38db9fd6b04f7883496add6cb5b
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-15 12:20:40 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -209,7 +209,8 @@ $preference->save();
                                         </h3>
                                     </div>
                                     <!--<button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>-->
-                                    <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>"></script>
+                                    <!--<script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>"></script>-->
+                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
                                 </div>
                             </div>
                         </div>
