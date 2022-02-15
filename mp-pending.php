83fed97fe9997ffd505d18f0117bce48df7132f0
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-15 16:35:53 -0300

    Update mp-pending.php

--- mp-pending.php
+++ mp-pending.php
@@ -1,6 +1,30 @@
 <?php
-  $req_dump = print_r($_REQUEST, TRUE);
-  $fp = fopen( __DIR__ .'/logs/pending.log', 'a');
+  ini_set('display_errors', '1');
+  ini_set('display_startup_errors', '1');
+  error_reporting(E_ALL);
+
+  $fp = fopen( __DIR__ .'/logs.log', 'a');
+
+fwrite($fp, "PENDING");
+fwrite($fp, "request");
+$req_dump = print_r($_REQUEST, TRUE);
+  fwrite($fp, $req_dump);
+
+  fwrite($fp, "get");
+
+  $req_dump = print_r($_GET, TRUE);
+  fwrite($fp, $req_dump);
+
+fwrite($fp, "post");
+  $req_dump = print_r($_POST, TRUE);
   fwrite($fp, $req_dump);
   fclose($fp);
+
+/*$actual = file_get_contents($fichero);
+// Añade una nueva persona al fichero
+$actual .= "John Smith\n";
+// Escribe el contenido al fichero
+echo var_dump(file_put_contents($fichero, $actual));*/
+
+
 ?>
