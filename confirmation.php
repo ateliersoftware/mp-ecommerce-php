d08aabcdbb2119ee453cbf3a88c13f5b97316d27
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-18 14:39:24 -0300

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -1,21 +1,56 @@
 <?php
+
+require __DIR__ . '/lib/Doctrine/Common/Lexer/AbstractLexer.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/NamedArgumentConstructorAnnotation.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/TokenParser.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/PhpParser.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/DocParser.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/DocLexer.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/Reader.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/AnnotationRegistry.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/AnnotationReader.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/Annotation.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/Annotation/Target.php';
+require __DIR__ . '/lib/Doctrine/Common/Annotations/ImplicitlyIgnoredAnnotationNames.php';
+require __DIR__ . '/MercadoPago/Version.php';
+require __DIR__ . '/MercadoPago/Entity.php';
+require __DIR__ . '/MercadoPago/SDK.php';
+require __DIR__ . '/MercadoPago/RestClient.php';
+require __DIR__ . '/MercadoPago/Http/HttpRequest.php';
+require __DIR__ . '/MercadoPago/Http/CurlRequest.php';
+require __DIR__ . '/MercadoPago/Config/AbstractConfig.php';
+require __DIR__ . '/MercadoPago/Config.php';
+require __DIR__ . '/MercadoPago/Manager.php';
+require __DIR__ . '/MercadoPago/MetaDataReader.php';
+require __DIR__ . '/MercadoPago/Entities/Preference.php';
+require __DIR__ . '/MercadoPago/Entities/Shared/Item.php';
+require __DIR__ . '/MercadoPago/Entities/Shared/Payer.php';
+require __DIR__ . '/MercadoPago/Entities/Shared/PaymentMethod.php';
+
+$url = "https://mp-certification.herokuapp.com/";
+
+// SDK de Mercado Pago
+//use MercadoPago\SDK as MercadoPago\SDK;
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+
+
   $fp = fopen( __DIR__ .'/logs.log', 'a');
 
 fwrite($fp, "CONFIRMATION AL WEBHOOK");
-/*fwrite($fp, "request");
-$req_dump = print_r($_REQUEST, TRUE);
-  fwrite($fp, $req_dump);
-  fwrite($fp, "get");
-  $req_dump = print_r($_GET, TRUE);
-  fwrite($fp, $req_dump);
-fwrite($fp, "post");
-  $req_dump = print_r($_POST, TRUE);
-  fwrite($fp, $req_dump);*/
 
 $resultado = array_merge($_REQUEST, $_GET);
 $resultado = array_merge($resultado, $_POST);
 $req_dump = print_r($resultado, TRUE);
 fwrite($fp, $req_dump);
+
+$info = MercadoPago\SDK::get('/merchant_orders/'.$resultado['id']);
+fwrite($info, var_dump($info));
+
   fclose($fp);
+
+
 ?>
 
