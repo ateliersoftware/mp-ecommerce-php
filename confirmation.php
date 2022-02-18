15ba4b9cb7e4c3de96ab02f4723bc7c22256b563
ateliersoftware <jose@ateliersoftware.com.ar>
2022-02-18 16:21:51 -0300

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -53,7 +53,7 @@ fwrite($fp, $req_dump);
 
 //$info = MercadoPago\SDK::post('/merchant_orders/'.$resultado['id']);
 $info = MercadoPago\SDK::get('/merchant_orders/4180348052');
-echo var_dump($info);
+echo (json_encode($info));
 $req_dump = print_r($info, true);
 //fwrite($fp, '----');
 fwrite($fp, $req_dump);
